2c956e9b8f9b4582134c53220cdd0c5277b098ec
Amy <wallheac@gmail.com>
2019-11-11 14:18:53 -0600

    prop destructuring

--- rjoke/src/Footer.js
+++ rjoke/src/Footer.js
@@ -4,10 +4,14 @@ import React from 'react';
  * Changed this to use ES6 arrow function and export default at the bottom of the class.
  * This is just another way to write a functional component.
  */
-const Footer = (props) => {
+
+/*
+* In this version I've just destructured the company prop, which makes it a little prettier
+*/
+const Footer = ({company}) => {
   const date = new Date().getFullYear();
 
-  return <h6>Copyright {props.company} (c) {date}</h6>
+  return <h6>Copyright {company} (c) {date}</h6>
 };
 //If you look in App.js, you'll see that this syntax (export default) means you don't need destructuring to do your import
 export default Footer;
